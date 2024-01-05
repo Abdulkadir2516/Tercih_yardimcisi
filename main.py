9da27837e5519d59ef3e6076774460af0d86b1d7
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2024-01-05 21:26:25 +0300

    main
    bolum kısımları başlandı

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+# pyuic5 untitled.ui - o pencere.py
 import sys
 
 import veri_getir
@@ -21,9 +21,34 @@ class window(QMainWindow, Ui_MainWindow):
         self.add_2.clicked.connect(self.append2)
         self.drop.clicked.connect(self.remove)
         self.drop_2.clicked.connect(self.remove2)
+        self.tabWidget.setCurrentIndex(0)
+
+        self.getir_1.clicked.connect(self.uni_bolumleri_getir)
+        self.getir_2.clicked.connect(self.bolum_hastane_getir)
 
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), "Şehirler")
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), "Üniversiteler")
+
+    def uni_bolumleri_getir(self):
+        sinav = self.exams.currentText()
+        # bolum
+
+
+        secilen_bolumler = self.selected_liste1.count()
+        print(secilen_bolumler)
+
+        for i in range(secilen_bolumler):
+            alan = str(self.selected_liste1.item(i).text())
+            alan = alan.translate(str.maketrans("üışçö","uisco"))
+            veri_getir.getir2(sinav,alan)
+
+
+
+
+    def bolum_hastane_getir(self):
+        pass
+
+
 
     def append(self):
         secilen = self.liste1.currentItem()
@@ -35,18 +60,18 @@ class window(QMainWindow, Ui_MainWindow):
         self.selected_liste2.addItem(secilen.text())
 
     def remove(self):
-        secilen = self.selected_liste1.currentItem().text()
+        self.selected_liste1.takeItem(self.selected_liste1.currentRow())
 
     def remove2(self):
-        secilen = self.liste1.currentItem()
-
-        self.selected_liste1.addItem(secilen.text())
+        self.selected_liste2.takeItem(self.selected_liste2.currentRow())
 
     def temizle(self):
         self.liste1.clear()
         self.liste.clear()
         self.selected_liste1.clear()
         self.selected_liste2.clear()
+
+
 
     def change(self, value):
         sinav = self.exams.itemText(value)
@@ -60,13 +85,18 @@ class window(QMainWindow, Ui_MainWindow):
         elif sinav == "YKS" or sinav == "DGS":
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), "Bölümler")
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), "Üniversiteler")
+            self.getir_1.setText("Üniversiteleri \n Getir")
+            self.getir_2.setText("Bölümleri \n Getir")
 
+            # !!!
             self.liste.addItems(veri_getir.getir(f"{sinav.lower()}/uni"))
             self.liste1.addItems(veri_getir.getir(f"{sinav.lower()}/bolum"))
 
         elif sinav == "TUS" or sinav == "DUS":
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), "Hastaneler")
             self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), "Bölümler")
+            self.getir_1.setText("Bölümleri \n Getir")
+            self.getir_2.setText("Hastaneleri \n Getir")
 
             self.liste1.addItems(veri_getir.getir(f"{sinav.lower()}/hastane"))
             self.liste.addItems(veri_getir.getir(f"{sinav.lower()}/bolum"))
--- veri_getir.py
+++ veri_getir.py
@@ -17,7 +17,24 @@ def getir(value):
 
     return cities
 
+def getir2(sinav_turu, alan):
+    alan2 = str(alan).lower().replace(" ", "-")
+    url2 = url + f"/{sinav_turu}/bolum/{alan2}"
+    print(url2)
+    istek = requests.get(url2)
+    html_icerik = istek.content
+    soup = BeautifulSoup(html_icerik, "html.parser")
+    sorgu = soup.find("tbody").find_all("tr")
+    bolum = []
+    print(sorgu[1])
 
+    for j in sorgu:
+        ifade = str(j.text).replace("\n", "")
+        print(ifade)
+        if len(ifade) > 2:
+            bolum.append(ifade)
+
+    print(bolum)
 
 
 
